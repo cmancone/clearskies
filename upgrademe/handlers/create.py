b39ee6780cfb84f49508a56a1d3a1a19e5000461
Conor Mancone <cmancone@gmail.com>
2021-02-07 14:07:03 -0500

    Create handler working and tested

--- upgrademe/handlers/create.py
+++ upgrademe/handlers/create.py
@@ -1,24 +1,11 @@
-from .base import Base
+from .write import Write
 from .exceptions import InputError
 from collections import OrderedDict
 
 
-class Create(Base):
-    _request = None
-    _models = None
-    _authentication = None
-    _writeable_columns = None
-    _readable_columns = None
-
-    _configuration_defaults = {
-        'columns': None,
-        'writeable_columns': None,
-        'readable_columns': None,
-    }
-
+class Create(Write):
     def __init__(self, request, authentication, models):
-        super().__init__(request, authentication)
-        self._models = models
+        super().__init__(request, authentication, models)
 
     def handle(self):
         model = self._models.empty_model()
@@ -33,84 +20,3 @@ class Create(Base):
         model.save(input_data)
 
         return self.success(self._model_as_json(model, columns))
-
-    def _check_configuration(self, configuration):
-        has_columns = 'columns' in configuration and configuration['columns'] is not None
-        has_writeable = 'writeable_columns' in configuration and configuration['writeable_columns'] is not None
-        has_readable = 'readable_columns' in configuration and configuration['readable_columns'] is not None
-        error_prefix = 'Configuration error for %s:' % (self.__class__.__name__)
-        if has_columns and has_writeable:
-            raise KeyError(f"{error_prefix} you must specify 'columns' OR 'writeable_columns', not both")
-        if has_columns and has_readable:
-            raise KeyError(f"{error_prefix} you must specify 'columns' OR 'readable_columns', not both")
-        if has_writeable and not has_readable:
-            raise KeyError(f"{error_prefix} you must specify 'readable_columns' if you specify 'writeable_columns'")
-        if has_readable and not has_writeable:
-            raise KeyError(f"{error_prefix} you must specify 'writeable_columns' if you specify 'readable_columns'")
-
-        for config_name in ['columns', 'writeable_columns', 'readable_columns']:
-            if config_name not in configuration or configuration[config_name] is not None:
-                continue
-            if type(configuration[config_name]) == list:
-                continue
-            raise ValueError(
-                f"{error_prefix} '{config_name}' should be a list of column names " +
-                f", not {str(type(configuration['columns']))}"
-            )
-
-        if has_columns and not configuration['columns']:
-            raise KeyError(f"{error_prefix} you must specify at least one column for 'columns'")
-        if has_writeable and not configuration['writeable_columns']:
-            raise KeyError(f"{error_prefix} you must specify at least one column for 'writeable_columns'")
-        if has_readable and not configuration['readable_columns']:
-            raise KeyError(f"{error_prefix} you must specify at least one column for 'readable_columns'")
-
-    def _get_rw_columns(self, columns, rw_type):
-        column_names = self.configuration('columns')
-        if column_names is None:
-            column_names = self.configuration(f'{rw_type}_columns')
-        wr_columns = OrderedDict()
-        for column_name in column_names:
-            if column_name not in columns:
-                class_name = self.__class__.__name__
-                model_class = self._models.model_class().__name__
-                raise ValueError(
-                    f"Handler {class_name} was configured with {rw_type} column '{column_name}' but this " +
-                    f"column doesn't exist for model {model_class}"
-                )
-            wr_columns[column_name] = columns[column_name]
-        return wr_columns
-
-    def _get_writeable_columns(self, columns):
-        if self._writeable_columns is None:
-            self._writeable_columns = self._get_rw_columns(columns, 'writeable')
-        return self._writeable_columns
-
-    def _get_readable_columns(self, columns):
-        if self._readable_columns is None:
-            self._readable_columns = self._get_rw_columns(columns, 'readable')
-        return self._readable_columns
-
-    def _extra_column_errors(self, input_data, columns):
-        input_errors = {}
-        allowed = self._get_writeable_columns(columns)
-        for column_name in input_data.keys():
-            if column_name not in allowed:
-                input_errors[column_name] = f"Input column '{column_name}' is not an allowed column"
-        return input_errors
-
-    def _find_input_errors(self, model, input_data, columns):
-        input_errors = {}
-        for column in self._get_writeable_columns(columns).values():
-            input_errors = {
-                **input_errors,
-                **column.input_errors(model, input_data),
-            }
-        return input_errors
-
-    def _model_as_json(self, model, columns):
-        json = OrderedDict()
-        json['id'] = int(model.id)
-        for column in self._get_readable_columns(columns).values():
-            json[column.name] = column.to_json(model)
-        return json
--- upgrademe/handlers/create_test.py
+++ upgrademe/handlers/create_test.py
@@ -3,7 +3,7 @@ from .create import Create
 from ..mocks import Models, Request
 from ..column_types import String, Integer
 from ..input_requirements import Required, MaximumLength
-from ..authentication import Public
+from ..authentication import Public, SecretBearer
 
 
 class CreateTest(unittest.TestCase):
@@ -35,3 +35,98 @@ class CreateTest(unittest.TestCase):
         self.assertEquals(10, response_data['age'])
         self.assertEquals('Conor', response_data['name'])
         self.assertEquals('c@example.com', response_data['email'])
+
+    def test_input_checks(self):
+        create = Create(
+            Request(json={'email': 'cmancone@example.com', 'age': 10}),
+            Public(),
+            self.models
+        )
+        create.configure({'columns': ['name', 'email', 'age']})
+        response = create()
+        self.assertEquals(200, response[1])
+        self.assertEquals(
+            {
+                'name': "'name' is required.",
+                'email': "'email' must be at most 15 characters long."
+            },
+            response[0]['inputErrors']
+        )
+
+    def test_columns(self):
+        self.models.add_create_response({
+            'id': 1,
+            'name': 'Conor',
+            'email': '',
+            'age': 10,
+        })
+
+        create = Create(
+            Request(json={'name': 'Conor', 'age': 10}),
+            Public(),
+            self.models
+        )
+        create.configure({'columns': ['name', 'age']})
+        response = create()
+        response_data = response[0]['data']
+        self.assertEquals(200, response[1])
+        self.assertEquals(1, response_data['id'])
+        self.assertEquals(10, response_data['age'])
+        self.assertTrue('email' not in response_data)
+        self.assertEquals({'name': 'Conor', 'age': 10}, self.models.created[0]['data'])
+
+    def test_extra_columns(self):
+        create = Create(
+            Request(json={'name': 'Conor', 'age': 10, 'email': 'hey', 'yo': 'sup'}),
+            Public(),
+            self.models
+        )
+        create.configure({'columns': ['name', 'age']})
+        response = create()
+        self.assertEquals(
+            {
+                'email': "Input column 'email' is not an allowed column",
+                'yo': "Input column 'yo' is not an allowed column",
+            },
+            response[0]['inputErrors']
+        )
+
+    def test_readable_writeable(self):
+        self.models.add_create_response({
+            'id': 1,
+            'name': 'Conor',
+            'email': 'default@email.com',
+            'age': 10,
+        })
+
+        create = Create(
+            Request(json={'name': 'Conor', 'age': 10}),
+            Public(),
+            self.models
+        )
+        create.configure({
+            'writeable_columns': ['name', 'age'],
+            'readable_columns': ['name', 'age', 'email'],
+        })
+        response = create()
+        response_data = response[0]['data']
+        self.assertEquals(200, response[1])
+        self.assertEquals(1, response_data['id'])
+        self.assertEquals(10, response_data['age'])
+        self.assertEquals('default@email.com', response_data['email'])
+        self.assertEquals({'name': 'Conor', 'age': 10}, self.models.created[0]['data'])
+
+    def test_auth(self):
+        create = Create(
+            Request(
+                json={'name': 'Conor', 'email': 'c@example.com', 'age': 10},
+                headers={'Authentication': 'Bearer qwerty'},
+            ),
+            SecretBearer('asdfer'),
+            self.models
+        )
+        create.configure({'columns': ['name', 'email', 'age']})
+        response = create()
+        self.assertEquals(401, response[1])
+        self.assertEquals('clientError', response[0]['status'])
+        self.assertEquals('Not Authorized', response[0]['error'])
