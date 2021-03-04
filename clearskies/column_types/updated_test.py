2532b333253532e897b7d2b7c56221cb09090334
Conor Mancone <cmancone@gmail.com>
2021-03-04 18:40:18 -0500

    Tests for created and updated

--- clearskies/column_types/updated_test.py
+++ clearskies/column_types/updated_test.py
@@ -1,23 +1,22 @@
 import unittest
-from .string import String
+from .updated import Updated
+import datetime
 
 
-class StringTest(unittest.TestCase):
-    def test_is_allowed_operator(self):
-        string = String()
-        for operator in ['=', 'LIKE']:
-            self.assertTrue(string.is_allowed_operator(operator))
-        for operator in ['==', '<=>']:
-            self.assertFalse(string.is_allowed_operator(operator))
+class UpdatedTest(unittest.TestCase):
+    def setUp(self):
+        self.now = datetime.datetime.strptime('2021-01-07 22:45:13', '%Y-%m-%d %H:%M:%S')
 
-    def test_build_condition(self):
-        string = String()
-        string.configure('name', {}, int)
-        self.assertEquals('name=sup', string.build_condition('sup', operator='='))
-        self.assertEquals("name LIKE '%asdf%'", string.build_condition('asdf', operator='like'))
-        self.assertEquals("name LIKE '%asdf%'", string.build_condition('asdf'))
+    def test_is_writeable(self):
+        updated = Updated(self.now)
+        self.assertFalse(updated.is_writeable)
 
-    def test_check_search_value(self):
-        string = String()
-        self.assertEquals('', string.check_search_value('sup'))
-        self.assertEquals('value should be a string', string.check_search_value(10))
+    def test_pre_save(self):
+        model = type('', (), {})
+        model.exists = False
+        updated = Updated(self.now)
+        updated.configure('updated', {}, int)
+        self.assertEquals({'hey': 'sup', 'updated': self.now}, updated.pre_save({'hey': 'sup'}, model))
+
+        model.exists = True
+        self.assertEquals({'hey': 'sup', 'updated': self.now}, updated.pre_save({'hey': 'sup'}, model))
