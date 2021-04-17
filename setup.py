d4122cecb926c19c541c05621df153ef297b835e
Conor Mancone <cmancone@gmail.com>
2021-04-17 19:45:02 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='clear-skies',
-    version='0.1.6',
+    version='0.1.7',
     description='A nano-framework for building microservices in the cloud',
     long_description=long_description,
     long_description_content_type='text/markdown',
