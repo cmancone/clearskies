f4f83573a69a828c6a86ae60369125ea35037e6e
Tom Nijboer <tom.nijboer@cimpress.com>
2022-12-21 15:41:28 +0100

    add profile for saml auth in Akeyless

--- src/clearskies/secrets/__init__.py
+++ src/clearskies/secrets/__init__.py
@@ -5,8 +5,8 @@ def akeyless(*args, **kwargs):
     return BindingConfig(AKeyless, *args, **kwargs)
 def akeyless_aws_iam_auth(access_id=None, api_host=None):
     return AKeylessAdditionalConfig('aws_iam', access_id=access_id, api_host=api_host)
-def akeyless_saml_auth(access_id=None, api_host=None):
-    return AKeylessAdditionalConfig('saml', access_id=access_id, api_host=api_host)
+def akeyless_saml_auth(access_id=None, api_host=None, profile=None):
+    return AKeylessAdditionalConfig('saml', access_id=access_id, api_host=api_host, profile=profile)
 def akeyless_jwt_auth(jwt_env_key, access_id=None, api_host=None):
     return AKeylessAdditionalConfig('jwt', jwt_env_key=jwt_env_key, access_id=access_id, api_host=api_host)
 def akeyless_access_key_auth(access_id=None, api_host=None):
