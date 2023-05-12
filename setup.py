e9dab84c11c7071782f08d0ebe4657df120ee2b0
Conor Mancone <cmancone@gmail.com>
2023-05-12 07:39:38 -0400

    src/clearskies/

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='clear-skies',
-    version='1.15.0',
+    version='1.15.1',
     description='A framework for building backends in the cloud',
     long_description=long_description,
     long_description_content_type='text/markdown',
