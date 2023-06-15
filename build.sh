e909dd8485f70d1224f4e90a08ce503c497e35d5
Conor Mancone <cmancone@gmail.com>
2023-06-15 10:57:58 -0400

    Switch to poetry

--- build.sh
+++ build.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
-# there does not appear to be a way to pass the token at run-time, so build our .pypirc file and delete when done
-echo '[pypi]' > ~/.pypirc
-echo 'username = __token__' >> ~/.pypirc
-echo "password = $(akeyless get-secret-value -n /pypi)" >> ~/.pypirc
-python3 -m build
-python3 -m twine upload dist/*
-rm ~/.pypirc
+poetry build
+poetry publish --username __token__ --password $(akeyless get-secret-value -n /pypi)
