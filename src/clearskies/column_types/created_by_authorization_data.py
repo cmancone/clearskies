471f72370254fec226c8de3b4df12eb09ac27ff1
Conor Mancone <cmancone@gmail.com>
2023-08-13 14:57:43 -0400

    Minor tweak

--- src/clearskies/column_types/created_by_authorization_data.py
+++ src/clearskies/column_types/created_by_authorization_data.py
@@ -18,4 +18,9 @@ class CreatedByAuthorizationData(String):
             return data
 
         authorization_data = self.di.build("input_output", cache=True).get_authorization_data()
-        return {**data, self.name: authorization_data.get(self.config("authorization_data_key_name"), "N/A")}
+        # data comes last so that it can override the info in the authorization data.  This seems counter-intuitive,
+        # but is important.  You would think that you *don't* want the data from the authorization data to be
+        # overridden (since this is mainly used for logging), but the trouble is that there are a variety of use-cases
+        # where the application must provide the audit data.  Examples include registration and login.  In these
+        # cases, authorization data will be empty, and must be provided by the applicaiton.
+        return {self.name: authorization_data.get(self.config("authorization_data_key_name"), "N/A"), **data}
