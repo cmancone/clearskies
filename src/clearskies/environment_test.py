4e28f7ad4e18a11ae7352473f2701a0677770f42
Conor Mancone <cmancone@gmail.com>
2025-06-13 15:34:05 -0400

    MOAR test cleanup

--- src/clearskies/environment_test.py
+++ src/clearskies/environment_test.py
@@ -8,7 +8,7 @@ class EnvironmentTest(unittest.TestCase):
     def setUp(self):
         self.secrets = type("", (), {"get": MagicMock(return_value="my_secret")})
         self.environment = Environment(
-            "%s/environment_test_file" % os.getcwd(),
+            "",
             {
                 "env_in_environment": "yup",
                 "also": "secret:///another/secret/path",
@@ -20,16 +20,9 @@ class EnvironmentTest(unittest.TestCase):
     def test_get_from_env(self):
         self.assertEqual("yup", self.environment.get("env_in_environment"))
 
-    def test_get_from_file(self):
-        self.assertEqual("sup", self.environment.get("anothervalue"))
-
     def test_get_from_env_resolve_secret(self):
         self.assertEqual("my_secret", self.environment.get("also"))
         self.secrets.get.assert_called_with("/another/secret/path")
-
-    def test_get_from_file_resolve_secret(self):
-        self.assertEqual("my_secret", self.environment.get("to_secrets"))
-        self.secrets.get.assert_called_with("/path/to/secret")
 
     def test_no_crash_for_ints(self):
         self.assertEqual(5, self.environment.get("an_integer"))
--- src/clearskies/functional/validations_test.py
+++ src/clearskies/functional/validations_test.py
@@ -1,27 +1,18 @@
+from types import SimpleNamespace
 import unittest
 from . import validations
-from ..model import Model
-
-
-class User(Model):
-    def __init__(self):
-        super().__init__("backend", "columns")
-
-    def columns_configuration(self):
-        return []
-
+from clearskies import Model
 
 class ValidationsTest(unittest.TestCase):
     def test_is_model(self):
-        self.assertTrue(validations.is_model(User()))
-        self.assertFalse(validations.is_model(""))
+        assert validations.is_model(SimpleNamespace(destination_name=True))
+        assert not validations.is_model("")
 
     def test_is_model_class(self):
-        self.assertTrue(validations.is_model_class(User))
-        self.assertFalse(validations.is_model_class(""))
-        self.assertFalse(validations.is_model_class(User()))
+        assert validations.is_model_class(Model)
+        assert not validations.is_model_class("")
 
     def test_is_model_or_class(self):
-        self.assertTrue(validations.is_model_or_class(User))
-        self.assertTrue(validations.is_model_or_class(User()))
-        self.assertFalse(validations.is_model_or_class(""))
+        assert validations.is_model_or_class(Model)
+        assert validations.is_model_or_class(SimpleNamespace(destination_name=True))
+        assert not validations.is_model_or_class("")
